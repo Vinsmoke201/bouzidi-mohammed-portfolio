f425737c39264a5d9ebb18821c8a63653a100bb1
Abdelilah AYACHE <a.ayache@netopia.ma>
2025-09-22 13:01:32 +0100

    changing the styling of the cards of the teams

--- app/(components)/service-section/ServiceSection.js
+++ app/(components)/service-section/ServiceSection.js
@@ -1,7 +1,6 @@
 'use client';
 
 import { motion } from "framer-motion";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { AiFillProduct } from "react-icons/ai";
 import { SiCssdesignawards } from "react-icons/si";
@@ -28,44 +27,20 @@ const services = [
 
 const ServiceCard = ({ service, index }) => {
   const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.2 });
-  const { t } = useTranslation("portfolio");
-  const [isHovered, setIsHovered] = useState(false);
 
-  // Define animation variants for the card
-  const cardVariants = {
-    initial: { opacity: 0, y: 30 },
-    animate: { opacity: 1, y: 0 },
-    hover: { scale: 1.05, transition: { duration: 0.3 } },
-    rest: { scale: 1, transition: { duration: 0.3 } },
-  };
+  const {t} = useTranslation("portfolio")
 
-  // Define animation variants for the background div
-  const bgVariants = {
-    rest: { rotate: 0, transition: { duration: 0.7 } },
-    hover: { rotate: -5, transition: { duration: 0.7 } },
-  };
 
   return (
     <motion.div
       ref={ref}
-      initial="initial"
-      animate={inView ? "animate" : "initial"}
-      variants={cardVariants}
-      whileHover="hover"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseOut={() => setIsHovered(false)}
+      initial={{ opacity: 0, y: 30 }}
+      animate={inView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.6, delay: index * 0.2 }}
       className="relative w-[20rem] h-[22rem] group"
     >
-      <motion.div
-        className="absolute bg-[#012C8D] w-full h-full rounded-md -z-10"
-        variants={bgVariants}
-        animate={isHovered ? "hover" : "rest"}
-      />
-      <div
-        className={`w-full h-full flex flex-col gap-6 transition-colors duration-700 bg-[#000B20] border-2 ${
-          isHovered ? "border-[#1059AD]" : "border-gray-200/20"
-        } rounded-md p-6`}
-      >
+      <div className="absolute bg-[#012C8D] w-full h-full group-hover:-rotate-5 transition-transform duration-700 -z-10 rounded-md" />
+      <div className="w-full h-full flex flex-col gap-6 transition-colors duration-700 bg-[#000B20] border-2 border-gray-200/20 hover:border-2 hover:border-[#1059AD] rounded-md p-6">
         {service.icon}
         <h2 className="text-xl font-bold">{t(service.nameKey)}</h2>
         <p className="text-justify text-gray-400">{t(service.descKey)}</p>
